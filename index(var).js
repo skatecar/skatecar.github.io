5171f747a5756191fed8a596e5d58d9df2ac1c08
Pear <skatecar001@live.com>
2020-11-30 14:23:32 -0600

    finally

--- index(var).js
+++
@@ -1,1 +0,0 @@
-var OEConfWECounter = {"WE6a2202f53d":{"ScriptVersion":22,"Config":{"TypeCounter":0,"InitialValue":0,"Lenght":4,"CountMethod":1}}}
--- index(var).php
+++
@@ -1,3 +0,0 @@
-<?php
-$OESiteUploadDirectory = "Upload";
-$OEConfWECounter = '{"WE6a2202f53d":{"ScriptVersion":22,"Config":{"TypeCounter":0,"InitialValue":0,"Lenght":4,"CountMethod":1}}}';
