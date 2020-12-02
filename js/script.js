79466a5498bafa08d4ee8f06ba2e80e4545a169b
Pear <skatecar001@live.com>
2020-12-01 21:23:46 -0600

    a

--- js/script.js
+++
@@ -1,4 +0,0 @@
-
-
-    window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-index","displayName":"Index","link":{"linkType":"LinkTypePage","href":"#!page-index"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-art","displayName":"Art","link":{"linkType":"LinkTypePage","href":"#!page-art"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false}]}
